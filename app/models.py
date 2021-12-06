09f2fbf90fe40eb6d8d9ca11597fc61a06c6d144
eduardoMag <eduardobautista13@gmail.com>
2021-12-05 19:12:28 -0600

    adding changes in code 8e

--- app/models.py
+++ app/models.py
@@ -1,12 +1,8 @@
-from . import db
+from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from . import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -26,6 +22,8 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
 
+    confirmed = db.Column(db.boolean, default=False)
+
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -37,5 +35,27 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id}).decode('utf-8')
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirm = True
+        db.session.add(self)
+        return True
+
+
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
--- flasky.py
+++ flasky.py
@@ -23,8 +23,3 @@ def test(test_names):
     else:
         tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-
-
-if __name__ == "__main__":
-    app.run()
-# page 114/258
